0dcf67ff6d720a9db36a55eb763fa7e8f7f314d9
Alaa7Hany <111649588+Alaa7Hany@users.noreply.github.com>
2025-05-05 18:25:59 +0300

    Update singleton.dart

--- NTI_02_task/singleton.dart
+++ NTI_02_task/singleton.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
 
 class MySingleton {
+  // make it static to access it in factory
   static final MySingleton instance = MySingleton._internal();
   int x = Random().nextInt(1000);
-
+// private named constructor 
+// prevent anyone from taking objects outside the class
   MySingleton._internal();
-
+// add factory to control what the constructors return every time it's used
   factory MySingleton() {
     return instance;
   }
